df6a6d6e52fc4d53515356963a0b4b47f4c20355
Tom Goldsmith <tom@7theta.com>
2021-02-06 19:37:55 -0500

    Use ventus-ui.

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject com.7theta/formal-ventus "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]
-                 [com.7theta/utilis "1.10.0"]
+                 [com.7theta/utilis "1.12.1"]
                  [com.7theta/formal "0.1.0"]
-                 [com.7theta/ventus "0.1.12"]
+                 [com.7theta/ventus-ui "0.1.0"]
                  [inflections "0.13.2"]
                  [reagent "1.0.0"]]
-  :profiles {:dev {:source-paths ["src" "example/src" "checkouts/formal/src" "checkouts/ventus/src"]
-                   :dependencies [[thheller/shadow-cljs "2.11.13"]]}})
+  :profiles {:dev {:source-paths ["src"
+                                  "example/src"
+                                  "../formal/src"
+                                  "../ventus-ui/src"]
+                   :dependencies [[thheller/shadow-cljs "2.11.17"]]}})
--- src/formal_ventus/core.cljs
+++ src/formal_ventus/core.cljs
@@ -1,10 +1,13 @@
 (ns formal-ventus.core
   (:require [formal.core :as f]
             [formal.util.component :as c]
-            [ventus.tailwind-ui :as tw]
-            [ventus.util.classes :refer [join-classes]]
+            [ventus-ui.input :as input]
+            [ventus-ui.select :as select]
+            [ventus-ui.switch :as switch]
+            [tailwind.core :refer [tw]]
             [utilis.js :as j]
             [utilis.types.number :refer [string->double string->long]]
+            [clojure.set :refer [rename-keys]]
             [inflections.core :as inflections]
             [reagent.core :as r]))
 
@@ -20,7 +23,7 @@
     (cond
       (and (sequential? error)
            (= 1 (count error)))
-      [tw/typography {:color :error}
+      [:p {:class-name (tw [:text-sm :text-red-600])}
        (->error-string (first error))]
 
       (map? error)
@@ -36,12 +39,12 @@
             (doall))]
 
       :else
-      [tw/typography {:color :error}
+      [:p {:class-name (tw [:text-sm :text-red-600])}
        (->error-string error)])))
 
 (defn form-label
   [{:keys [props]}]
-  (into [:label (merge {:class-name "block text-sm font-medium text-gray-700"} props)]
+  (into [:label (merge {:class-name (tw [:block :text-sm :font-medium :text-gray-700])} props)]
         (r/children (r/current-component))))
 
 (defn form
@@ -59,8 +62,8 @@
                     {:keys [input-id]} (r/state this)
                     label (or label (when id (labelize id)))
                     label (if required (str label "*") label)]
-                [:div {:class-name "py-2"}
-                 [:div {:class-name input-container-classes}
+                [:div {:class-name (tw [:py-2])}
+                 [:div {:class-name (tw input-container-classes)}
                   (when (and label (= label-position :start))
                     [form-label {:for input-id} label])
                   (when-let [input (first (r/children this))]
@@ -81,23 +84,23 @@
   [{:keys [on-change] :as props}]
   (let [{:keys [error] :as props} (update-error props)]
     [form-item props
-     [tw/input (-> props
-                   (select-keys [:default-value :placeholder])
-                   (assoc :type :text
-                          :error (boolean error)
-                          :on-change #(on-change (j/get-in % [:currentTarget :value]))))]]))
+     [input/input (-> props
+                      (select-keys [:default-value :placeholder])
+                      (assoc :type :text
+                             :validation (when error {:status :error})
+                             :on-change #(on-change (j/get-in % [:currentTarget :value]))))]]))
 
 (defn integer-input
   [{:keys [on-change] :as props}]
   (let [{:keys [error] :as props} (update-error props)]
     [form-item props
-     [tw/input (-> props
-                   (select-keys [:default-value :placeholder])
-                   (assoc :type :number
-                          :error (boolean error)
-                          :on-change #(on-change
-                                       (string->long
-                                        (j/get-in % [:currentTarget :value])))))]]))
+     [input/input (-> props
+                      (select-keys [:default-value :placeholder])
+                      (assoc :type :number
+                             :validation (when error {:status :error})
+                             :on-change #(on-change
+                                          (string->long
+                                           (j/get-in % [:currentTarget :value])))))]]))
 
 (defn boolean-input
   [{:keys [on-change default-value] :as props}]
@@ -106,21 +109,22 @@
         {:keys [error] :as props} (update-error props)]
     [form-item (assoc props
                       :label-position :end
-                      :input-container-classes "flex justify-start items-center")
-     [tw/switch {:checked (boolean checked)
-                 :class-name :mr-1
-                 :on-change #(let [checked (not checked)]
-                               (on-change checked)
-                               (r/set-state this {:checked checked}))}]]))
+                      :input-container-classes [:flex :justify-start :items-center])
+     [switch/switch {:checked (boolean checked)
+                     :on-change #(let [checked (not checked)]
+                                   (on-change checked)
+                                   (r/set-state this {:checked checked}))}]]))
 
 (defn enum-input
   [{:keys [on-change error children] :as props}]
   (let [this (r/current-component)
-        {:keys [default-handled]} (r/state this)
         {:keys [error] :as props} (update-error props)
         options (map (fn [option]
                        (let [id (str option)]
                          {:id id :value option})) children)
+        {:keys [default-handled selected-id]
+         :or {selected-id (or (:default-value props)
+                              (:id (first options)))}} (r/state this)
         lookup (->> options
                     (map (fn [{:keys [id] :as option}]
                            [id option]))
@@ -131,36 +135,33 @@
           (on-change value)))
       (r/set-state this {:default-handled true}))
     [form-item props
-     [tw/select (-> props
-                    (select-keys [:default-value])
-                    (assoc :on-change #(on-change (:value (get lookup %)))))
-      (->> options
-           (map-indexed
-            (fn [index {:keys [id value]}]
-              [tw/select-option
-               {:key (str "option-" index "-" id)
-                :value id}
-               (labelize value)]))
-           (doall))]]))
+     (into [select/select {:selected-id selected-id
+                           :on-change #(do (r/set-state this {:selected-id %})
+                                           (on-change (:value (get lookup %))))}]
+           (->> options
+                (map-indexed
+                 (fn [index {:keys [id value]}]
+                   [select/option
+                    {:key (str "option-" index "-" id)
+                     :id id}
+                    (labelize value)]))
+                (doall)))]))
 
 (defn map-input
   [{:keys [index value error inputs id title subtitle]}]
-  [:div {:class-name (join-classes
-                      :p-4
-                      (when (and index (pos? index)) :mt-2))}
+  [:div {:class-name (tw (concat
+                          [:p-4]
+                          (when (and index (pos? index))
+                            [:mt-2])))}
    [:div
     (when title
-      [tw/typography {:variant :h3
-                      :text-size :lg
-                      :classes {:text-color [:leadinfg-6 :font-medium :text-gray-900]}}
+      [:h3 {:class-name (tw [:leading-6 :font-medium :text-gray-900 :text-lg])}
        title])
     (when subtitle
-      [tw/typography {:variant :p
-                      :text-size :sm
-                      :classes {:text-color :text-gray-500}}
+      [:p {:class-name (tw [:text-gray-500 :text-sm])}
        subtitle])]
    (into
-    [:div {:class-name (when (or title subtitle) :mt-2)}]
+    [:div {:class-name (tw (when (or title subtitle) [:mt-2]))}]
     (concat
      (->> inputs
           (sort-by (comp :index :props second))
@@ -168,7 +169,7 @@
                  [render props]))
           (doall))
      (when (and error (seq value))
-       [[:div {:class-name :mt-2}
+       [[:div {:class-name (tw [:mt-2])}
          [error-typography {:error error}]]])))])
 
 (defn sequential-input
@@ -180,7 +181,7 @@
                      (doall))
                 [[:button {:on-click #(on-change (conj value nil))} "Add"]]
                 (when error
-                  [[:div {:class-name :mt-2}
+                  [[:div {:class-name (tw [:mt-2])}
                     [error-typography {:error error}]]]))))
 
 (f/reg-input :ventus/int integer-input)
