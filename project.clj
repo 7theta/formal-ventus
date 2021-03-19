10712713851b06e2ccce33668108782a1074bb0e
C.T. Paterson <ct@7theta.com>
2021-03-19 09:48:42 -0400

    Update ventus-ui dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.7theta/formal-ventus "0.1.1"
+(defproject com.7theta/formal-ventus "0.1.2"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.773"]
                  [com.7theta/utilis "1.12.1"]
                  [com.7theta/formal "0.1.1"]
-                 [com.7theta/ventus-ui "0.1.0"]
+                 [com.7theta/ventus-ui "0.2.1"]
                  [inflections "0.13.2"]
                  [reagent "1.0.0"]]
   :profiles {:dev {:source-paths ["src"
