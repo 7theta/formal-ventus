5e4bcd69fe5ce021227a40f1a621eb24b1906ff4
Joshua DeGagné <josh@Joshuas-MacBook-Pro.local>
2021-06-02 17:04:59 -0400

    upversion ventus-ui

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                  [org.clojure/clojurescript "1.10.773"]
                  [com.7theta/utilis "1.12.1"]
                  [com.7theta/formal "0.1.2"]
-                 [com.7theta/ventus-ui "0.1.0"]
+                 [com.7theta/ventus-ui "0.2.2"]
                  [inflections "0.13.2"]
                  [reagent "1.0.0"]]
   :profiles {:dev {:source-paths ["src"
